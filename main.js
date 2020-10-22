3cea285c844440c27fa93f580a62f177ddd7747b
zacherysims <zach.md.sims97@gmail.com>
2020-10-21 20:17:06 -0600

    done?

---
+++ main.js
@@ -0,0 +1,361 @@
+"use strict";
+
+window.addEventListener('load', main);
+
+var touchTimer;
+
+const STATE_HIDDEN = "hidden";
+const STATE_SHOWN = "shown";
+const STATE_MARKED = "marked";
+
+function array2d(nrows, ncols, val) {
+    const res = [];
+    for (let row = 0; row < nrows; row++) {
+        res[row] = [];
+        for (let col = 0; col < ncols; col++)
+            res[row][col] = val(row, col);
+    }
+    return res;
+}
+
+function rndInt(min, max) {
+    [min, max] = [Math.ceil(min), Math.floor(max)]
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+function prepare_dom(s) {
+    const grid = document.querySelector(".grid");
+    const nCards = 24 * 20; // max grid size
+    for (let i = 0; i < nCards; i++) {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.setAttribute("data-cardInd", i);
+        card.oncontextmenu = function () { return false; };
+        card.addEventListener("mousedown", (event) => {
+            handleCellClick(event, s, i);
+        });
+        card.addEventListener("touchstart", () => {
+            touchTimer = setTimeout(() => {
+                game.mark(i);
+                touchTimer = null;
+            }, 1000);
+        });
+        card.addEventListener("touchend", () => {
+            if (touchTimer) {
+                clearTimeout(touchTimer);
+                touchTimer = null;
+                s.uncover(i);
+            }
+        });
+        grid.appendChild(card);
+    }
+}
+
+function handleCellClick(event, s, ind) {
+    if (event.buttons == 1) {
+        card_click_cb(s, ind);
+    }
+    else if (event.buttons == 2) {
+        mark(s, ind);
+    }
+}
+
+function validCoord(s, row, col) {
+    return row >= 0 && row < s.rows && col >= 0 && col < s.cols;
+}
+
+function count(s, row, col) {
+    const c = (s, r, c) =>
+        (validCoord(s, r, c) && s.cells[r][c].mine ? 1 : 0);
+    let res = 0;
+    for (let dr = -1; dr <= 1; dr++)
+        for (let dc = -1; dc <= 1; dc++)
+            res += c(s, row + dr, col + dc);
+    return res;
+}
+
+function sprinkleMines(s, ind) {
+
+    const col = ind % s.cols;
+    const row = Math.floor(ind / s.cols);
+
+    let allowed = [];
+    for (let r = 0; r < s.rows; r++) {
+        for (let c = 0; c < s.cols; c++) {
+            if (Math.abs(row - r) > 2 || Math.abs(col - c) > 2)
+                allowed.push([r, c]);
+        }
+    }
+    s.minecount = Math.min(s.minecount, allowed.length);
+    for (let i = 0; i < s.minecount; i++) {
+        let j = rndInt(i, allowed.length - 1);
+        [allowed[i], allowed[j]] = [allowed[j], allowed[i]];
+        let [r, c] = allowed[i];
+        s.cells[r][c].mine = true;
+    }
+
+    for (let r = 0; r < s.rows; r++) {
+        for (let c = 0; c < s.cols; c++) {
+            if (s.cells[r][c].state == STATE_MARKED) {
+                s.cells[r][c].state = STATE_HIDDEN;
+                s.markedcount--;
+            }
+            s.cells[r][c].count = count(s, r, c);
+        }
+    }
+    let mines = []; let counts = [];
+    for (let row = 0; row < s.rows; row++) {
+        let str = "";
+        for (let col = 0; col < s.cols; col++) {
+            str += s.cells[row][col].mine ? "B" : ".";
+        }
+        str += "  |  ";
+        for (let col = 0; col < s.cols; col++) {
+            str += s.cells[row][col].count.toString();
+        }
+        mines[row] = str;
+    }
+    console.log("Mines and counts after sprinkling:");
+    console.log(mines.join("\n"), "\n");
+}
+
+function uncover(s, ind) {
+    const col = ind % s.cols;
+    const row = Math.floor(ind / s.cols);
+
+    if (!validCoord(s, row, col)) return false;
+    if (s.uncoveredcount === 0)
+        sprinkleMines(s, ind);
+    // if cell is not hidden, ignore this move
+    if (s.cells[row][col].state !== STATE_HIDDEN) return false;
+    // floodfill all 0-count cells
+    const ff = (r, c) => {
+        if (!validCoord(s, r, c)) return;
+        if (s.cells[r][c].state !== STATE_HIDDEN) return;
+        s.cells[r][c].state = STATE_SHOWN;
+        s.uncoveredcount++;
+        if (s.cells[r][c].count !== 0) return;
+        ff(r - 1, c - 1); ff(r - 1, c); ff(r - 1, c + 1);
+        ff(r, c - 1);; ff(r, c + 1);
+        ff(r + 1, c - 1); ff(r + 1, c); ff(r + 1, c + 1);
+    };
+    ff(row, col);
+    // have we hit a mine?
+    if (s.cells[row][col].mine) {
+        console.log("we esploded");
+        s.exploded = true;
+    }
+    render(s);
+    console.log(getRendering(s));
+    console.log("%d have been uncovered out of a total of %d cells (%d uncleared cells)", s.uncoveredcount, s.rows * s.cols, (s.rows * s.cols - s.uncoveredcount - s.minecount));
+    return true;
+}
+
+function mark(s, ind) {
+    const col = ind % s.cols;
+    const row = Math.floor(ind / s.cols);
+    // if coordinates invalid, refuse this request
+    if (!validCoord(s, row, col)) return false;
+    if (s.markedcount == s.minecount && s.cells[row][col].state != STATE_MARKED) return false;
+    // if cell already uncovered, refuse this
+    if (s.cells[row][col].state === STATE_SHOWN) return false;
+    // accept the move and flip the marked status
+    s.markedcount += s.cells[row][col].state == STATE_MARKED ? -1 : 1;
+    s.cells[row][col].state = s.cells[row][col].state == STATE_MARKED ?
+        STATE_HIDDEN : STATE_MARKED;
+    render(s);
+    return true;
+}
+
+function card_click_cb(s, ind) {
+    const col = ind % s.cols;
+    const row = Math.floor(ind / s.cols);
+    console.log("left click at index %d which has %d mines", ind, s.cells[row][col].count);
+    uncover(s, ind);
+    document.querySelectorAll(".endTime").forEach(
+        (e)=> {
+          let end = new Date();
+          e.textContent = String(Math.floor((end-s.start)/1000));
+      })
+    if (s.uncoveredcount == (s.cols * s.rows - s.minecount)) {
+        document.querySelector("#overlaywin").classList.toggle("active");
+    }
+    if (s.exploded) {
+        document.querySelector("#overlayloss").classList.toggle("active");
+    }
+}
+
+function menu_button_cb(s, cols, rows) {
+    let mines = 0;
+    if (cols == 10)
+        mines = 10;
+    else if (cols == 18)
+        mines = 40;
+    else if (cols == 24)
+        mines = 99;
+    init(s, cols, rows, mines);
+    clear(s);
+    render(s);
+}
+
+
+function render(s) {
+    const grid = document.querySelector(".grid");
+    grid.style.gridTemplateColumns = `repeat(${s.cols}, 1fr)`;
+    for (let i = 0; i < grid.children.length; i++) {
+        const card = grid.children[i];
+        const ind = Number(card.getAttribute("data-cardInd"));
+        const col = ind % s.cols;
+        const row = Math.floor(ind / s.cols);
+        if (ind >= s.rows * s.cols) {
+            card.style.display = "none";
+        }
+        else {
+            card.style.display = "block";
+            if (s.cells[row][col].state == STATE_SHOWN) {
+                card.classList.add("shown");
+                if (!s.cells[row][col].mine == true) {
+                    switch (s.cells[row][col].count) {
+                        case 1:
+                            card.classList.add("one");
+                            break;
+                        case 2:
+                            card.classList.add('two');
+                            break;
+                        case 3:
+                            card.classList.add('three');
+                            break;
+                        case 4:
+                            card.classList.add('four');
+                            break;
+                        case 5:
+                            card.classList.add('five');
+                            break;
+                        case 6:
+                            card.classList.add('six');
+                            break;
+                        case 7:
+                            card.classList.add('seven');
+                            break;
+                        case 8:
+                            card.classList.add('eight');
+                            break;
+                        default:
+                            card.classList.add('none');
+                            break;
+                    }
+                }
+            }
+            if (s.exploded && s.cells[row][col].mine == true) {
+                card.classList.remove("shown");
+                card.classList.add("exploded");
+
+            }
+            if (s.cells[row][col].state == STATE_MARKED)
+                card.classList.add("marked");
+            else
+                card.classList.remove("marked");
+        }
+    }
+    document.querySelectorAll(".mineCount").forEach(
+        (e) => {
+            e.textContent = String(s.minecount);
+        });
+
+    document.querySelectorAll(".flagCount").forEach(
+        (e) => {
+            e.textContent = String(s.minecount - s.markedcount);
+        });
+}
+
+function getRendering(s) {
+    const res = [];
+    for (let row = 0; row < s.rows; row++) {
+        let str = "";
+        for (let col = 0; col < s.cols; col++) {
+            let a = s.cells[row][col];
+            if (s.exploded && a.mine) str += "M";
+            else if (a.state === STATE_HIDDEN) str += "H";
+            else if (a.state === STATE_MARKED) str += "F";
+            else if (a.mine) str += "M";
+            else str += a.count.toString();
+        }
+        res[row] = str;
+    }
+    return res;
+}
+
+function clear(s) {
+    const grid = document.querySelector(".grid");
+    grid.style.gridTemplateColumns = `repeat(${s.cols}, 1fr)`;
+    for (let i = 0; i < grid.children.length; i++) {
+        const card = grid.children[i];
+        card.classList.remove("uncovered");
+        card.classList.remove("marked");
+        card.classList.remove("exploded");
+        card.classList.remove("none");
+        card.classList.remove("one");
+        card.classList.remove("two");
+        card.classList.remove("three");
+        card.classList.remove("four");
+        card.classList.remove("five");
+        card.classList.remove("six");
+        card.classList.remove("seven");
+        card.classList.remove("eight");
+        card.innerHTML = "";
+    }
+}
+
+function init(s, cols, rows, mines) {
+    s.cols = cols;
+    s.rows = rows;
+    s.minecount = mines;
+    s.uncoveredcount = 0;
+    s.markedcount = 0;
+    s.exploded = false;
+    s.start = new Date();
+    s.cells = array2d(rows, cols, () => ({ mine: false, state: STATE_HIDDEN, count: 0 }));
+
+}
+
+function main() {
+
+    let state = {
+        cols: 10,
+        rows: 8,
+        minecount: 10,
+        uncoveredcount: 0,
+        markedcount: 0,
+        exploded: false,
+        start: null,
+        cells: array2d(8, 10, () => ({ mine: false, state: STATE_HIDDEN, count: 0 }))
+    }
+
+    document.querySelectorAll(".menuButton").forEach((button) => {
+        let [rows, cols] = button.getAttribute("data-size").split("x").map(s => Number(s));
+        button.innerHTML = `${cols} &#x2715; ${rows}`
+        button.addEventListener("click", menu_button_cb.bind(null, state, cols, rows));
+    });
+
+    document.querySelector("#overlaywin").addEventListener("click", () => {
+        document.querySelector("#overlaywin").classList.remove("active");
+        menu_button_cb(state, state.cols, state.rows);
+    });
+
+    // callback for overlay click - hide overlay and regenerate game
+    document.querySelector("#overlayloss").addEventListener("click", () => {
+        document.querySelector("#overlayloss").classList.remove("active");
+        menu_button_cb(state, state.cols, state.rows);
+    });
+
+    prepare_dom(state);
+    init(state, 10, 8, 10);
+    render(state);
+    setInterval(() => {
+        document.querySelectorAll(".time").forEach(
+            (e)=> {
+              let end = new Date();
+              e.textContent = String(Math.floor((end-state.start)/1000))+" seconds";
+          });
+      }, 500);
+}
---
+++ pavols-main.js
@@ -0,0 +1,201 @@
+"use strict";
+
+let MSGame = (function(){
+
+  // private constants
+  const STATE_HIDDEN = "hidden";
+  const STATE_SHOWN = "shown";
+  const STATE_MARKED = "marked";
+
+  function array2d( nrows, ncols, val) {
+    const res = [];
+    for( let row = 0 ; row < nrows ; row ++) {
+      res[row] = [];
+      for( let col = 0 ; col < ncols ; col ++)
+        res[row][col] = val(row,col);
+    }
+    return res;
+  }
+
+  // returns random integer in range [min, max]
+  function rndInt(min, max) {
+    [min,max] = [Math.ceil(min), Math.floor(max)]
+    return min + Math.floor(Math.random() * (max - min + 1));
+  }
+
+  class _MSGame {
+    constructor() {
+      this.init(8,10,10); // easy
+    }
+
+    validCoord(row, col) {
+      return row >= 0 && row < this.nrows && col >= 0 && col < this.ncols;
+    }
+
+    init(nrows, ncols, nmines) {
+      this.nrows = nrows;
+      this.ncols = ncols;
+      this.nmines = nmines;
+      this.nmarked = 0;
+      this.nuncovered = 0;
+      this.exploded = false;
+      // create an array
+      this.arr = array2d(
+        nrows, ncols,
+        () => ({mine: false, state: STATE_HIDDEN, count: 0}));
+    }
+
+    count(row,col) {
+      const c = (r,c) =>
+            (this.validCoord(r,c) && this.arr[r][c].mine ? 1 : 0);
+      let res = 0;
+      for( let dr = -1 ; dr <= 1 ; dr ++ )
+        for( let dc = -1 ; dc <= 1 ; dc ++ )
+          res += c(row+dr,col+dc);
+      return res;
+    }
+    sprinkleMines(row, col) {
+        // prepare a list of allowed coordinates for mine placement
+      let allowed = [];
+      for(let r = 0 ; r < this.nrows ; r ++ ) {
+        for( let c = 0 ; c < this.ncols ; c ++ ) {
+          if(Math.abs(row-r) > 2 || Math.abs(col-c) > 2)
+            allowed.push([r,c]);
+        }
+      }
+      this.nmines = Math.min(this.nmines, allowed.length);
+      for( let i = 0 ; i < this.nmines ; i ++ ) {
+        let j = rndInt(i, allowed.length-1);
+        [allowed[i], allowed[j]] = [allowed[j], allowed[i]];
+        let [r,c] = allowed[i];
+        this.arr[r][c].mine = true;
+      }
+      // erase any marks (in case user placed them) and update counts
+      for(let r = 0 ; r < this.nrows ; r ++ ) {
+        for( let c = 0 ; c < this.ncols ; c ++ ) {
+          if(this.arr[r][c].state == STATE_MARKED)
+            this.arr[r][c].state = STATE_HIDDEN;
+          this.arr[r][c].count = this.count(r,c);
+        }
+      }
+      let mines = []; let counts = [];
+      for(let row = 0 ; row < this.nrows ; row ++ ) {
+        let s = "";
+        for( let col = 0 ; col < this.ncols ; col ++ ) {
+          s += this.arr[row][col].mine ? "B" : ".";
+        }
+        s += "  |  ";
+        for( let col = 0 ; col < this.ncols ; col ++ ) {
+          s += this.arr[row][col].count.toString();
+        }
+        mines[row] = s;
+      }
+      console.log("Mines and counts after sprinkling:");
+      console.log(mines.join("\n"), "\n");
+    }
+    // uncovers a cell at a given coordinate
+    // this is the 'left-click' functionality
+    uncover(row, col) {
+      console.log("uncover", row, col);
+      // if coordinates invalid, refuse this request
+      if( ! this.validCoord(row,col)) return false;
+      // if this is the very first move, populate the mines, but make
+      // sure the current cell does not get a mine
+      if( this.nuncovered === 0)
+        this.sprinkleMines(row, col);
+      // if cell is not hidden, ignore this move
+      if( this.arr[row][col].state !== STATE_HIDDEN) return false;
+      // floodfill all 0-count cells
+      const ff = (r,c) => {
+        if( ! this.validCoord(r,c)) return;
+        if( this.arr[r][c].state !== STATE_HIDDEN) return;
+        this.arr[r][c].state = STATE_SHOWN;
+        this.nuncovered ++;
+        if( this.arr[r][c].count !== 0) return;
+        ff(r-1,c-1);ff(r-1,c);ff(r-1,c+1);
+        ff(r  ,c-1);         ;ff(r  ,c+1);
+        ff(r+1,c-1);ff(r+1,c);ff(r+1,c+1);
+      };
+      ff(row,col);
+      // have we hit a mine?
+      if( this.arr[row][col].mine) {
+        this.exploded = true;
+      }
+      return true;
+    }
+    // puts a flag on a cell
+    // this is the 'right-click' or 'long-tap' functionality
+    mark(row, col) {
+      console.log("mark", row, col);
+      // if coordinates invalid, refuse this request
+      if( ! this.validCoord(row,col)) return false;
+      // if cell already uncovered, refuse this
+      console.log("marking previous state=", this.arr[row][col].state);
+      if( this.arr[row][col].state === STATE_SHOWN) return false;
+      // accept the move and flip the marked status
+      this.nmarked += this.arr[row][col].state == STATE_MARKED ? -1 : 1;
+      this.arr[row][col].state = this.arr[row][col].state == STATE_MARKED ?
+        STATE_HIDDEN : STATE_MARKED;
+      return true;
+    }
+    // returns array of strings representing the rendering of the board
+    //      "H" = hidden cell - no bomb
+    //      "F" = hidden cell with a mark / flag
+    //      "M" = uncovered mine (game should be over now)
+    // '0'..'9' = number of mines in adjacent cells
+    getRendering() {
+      const res = [];
+      for( let row = 0 ; row < this.nrows ; row ++) {
+        let s = "";
+        for( let col = 0 ; col < this.ncols ; col ++ ) {
+          let a = this.arr[row][col];
+          if( this.exploded && a.mine) s += "M";
+          else if( a.state === STATE_HIDDEN) s += "H";
+          else if( a.state === STATE_MARKED) s += "F";
+          else if( a.mine) s += "M";
+          else s += a.count.toString();
+        }
+        res[row] = s;
+      }
+      return res;
+    }
+    getStatus() {
+      let done = this.exploded ||
+          this.nuncovered === this.nrows * this.ncols - this.nmines;
+      return {
+        done: done,
+        exploded: this.exploded,
+        nrows: this.nrows,
+        ncols: this.ncols,
+        nmarked: this.nmarked,
+        nuncovered: this.nuncovered,
+        nmines: this.nmines
+      }
+    }
+  }
+
+  return _MSGame;
+
+})();
+
+let game = new MSGame();
+
+game.init(8, 10, 10);
+console.log(game.getRendering().join("\n"));
+console.log(game.getStatus());
+
+game.uncover(2,5);
+console.log(game.getRendering().join("\n"));
+console.log(game.getStatus());
+
+game.uncover(5,5);
+console.log(game.getRendering().join("\n"));
+console.log(game.getStatus());
+
+game.mark(4,5);
+console.log(game.getRendering().join("\n"));
+console.log(game.getStatus());
+
+
+console.log("end");
+
